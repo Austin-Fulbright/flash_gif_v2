fbaa9f2b99c90b07787ab52de374e0be003db3e7
Austin Fulbright <austinfulbright@Austins-MacBook-Pro.local>
2023-07-11 22:12:30 -0500

    added server for editing gif

--- js/editGifPanel.js
+++ js/editGifPanel.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
   // Get base64VideoData and set the video source
   chrome.runtime.sendMessage({ name: 'editGifOpen' }, function(response) {
@@ -13,13 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   // Add click event listener to cropButton
-  document.getElementById('cropButton').addEventListener('click', function() {
+  document.getElementById('cropButton').addEventListener('click', async function() {
       const cropValues = calculateCropValues();
       const video = document.getElementById('video');
       const base64VideoData = video.src;
       
       // Crop the video
-      const croppedVideoData = cropVideoWithFfmpeg(base64VideoData, cropValues.cropX, cropValues.cropY, cropValues.cropWidth, cropValues.cropHeight);
+      const croppedVideoData = await cropVideoWithFfmpeg(base64VideoData, cropValues.cropX, cropValues.cropY, cropValues.cropWidth, cropValues.cropHeight);
       
       // Set the src of the video element to the cropped video
       video.src = croppedVideoData;
@@ -31,15 +33,49 @@ function calculateCropValues() {
   const video = document.getElementById('video');
 
   return {
-      cropX: cropArea.offsetLeft / video.videoWidth,
-      cropY: cropArea.offsetTop / video.videoHeight,
-      cropWidth: cropArea.offsetWidth / video.videoWidth,
-      cropHeight: cropArea.offsetHeight / video.videoHeight,
+    cropX: cropArea.offsetLeft,
+    cropY: cropArea.offsetTop,
+    cropWidth: cropArea.offsetWidth,
+    cropHeight: cropArea.offsetHeight,
   };
+  
 }
 
-function cropVideoWithFfmpeg(base64VideoData, cropX, cropY, cropWidth, cropHeight) {
-  // This is a placeholder - you'll need to replace this with actual ffmpeg.js code
-  // Implement this function with ffmpeg or another type of cropping algorithm
-  return base64VideoData;
+async function cropVideoWithFfmpeg(base64VideoData, cropX, cropY, cropWidth, cropHeight) {
+  // Convert base64 data to Blob
+  const binary = atob(base64VideoData.split(',')[1]);
+  let array = [];
+  for (let i = 0; i < binary.length; i++) {
+    array.push(binary.charCodeAt(i));
+  }
+  let uint8Array = new Uint8Array(array);
+  const blob = new Blob([uint8Array], { type: 'video/mp4' });
+
+  // Create a FormData object
+  const formData = new FormData();
+  formData.append('video', blob, 'input.mp4');
+  formData.append('cropX', cropX);
+  formData.append('cropY', cropY);
+  formData.append('cropWidth', cropWidth);
+  formData.append('cropHeight', cropHeight);
+
+  // Send a POST request to the server
+  const response = await fetch('http://localhost:3000/crop', {
+    method: 'POST',
+    body: formData
+  });
+
+  // Check the response
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  // Get the response data as a Blob
+  const data = await response.blob();
+
+  // Convert it to a URL
+  const video = URL.createObjectURL(data);
+
+  return video;
 }
+
